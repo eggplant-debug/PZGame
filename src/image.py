c0aee8bfde90567a37367e0562b4da7fed6bfc01
centerlv <centerlv@tencent.com>
2024-06-27 22:40:50 +0800

    增加了帧动画

--- src/image.py
+++ src/image.py
@@ -2,11 +2,44 @@ import pygame
 
 
 class Image(pygame.sprite.Sprite):
-    def __init__(self, path,size) -> None:
-        self.path = path
+    def __init__(self, pathFmt,pathIndex,pos,size=None,pathIndexCount=0) -> None:
+        self.pathFmt = pathFmt
+        self.pathIndex = pathIndex
         self.size = size
-        self.image = pygame.image.load(path)
+        self.pos = list(pos)
+        self.pathIndexCount=pathIndexCount
         #缩放图片对象
-        self.image= pygame.transform.scale(self.image, self.size)
+        self.updateimage()
+
+    #index改变需要调upateimage
+    def updateIndex(self,pathIndex):
+        self.pathIndex=pathIndex
+        self.updateimage()
+
+    #size改变需要调upateimage
+    def updateSize(self,size):
+        self.size=size
+        self.updateimage()
+
+
+    def updateimage(self):
+        path = self.pathFmt
+        if self.pathIndexCount!=0:
+            path = path % self.pathIndex 
+        self.image = pygame.image.load(path)
+        if self.size!=None:
+            self.image= pygame.transform.scale(self.image, self.size)
+
+    def getRect(self):
+        rect = self.image.get_rect()
+        rect.x,rect.y = self.pos
+        return rect
+
+    #僵尸做移动的功能
+    def doleft(self, speed):
+        # 注意 pos不能是tuple，要用list
+        self.pos[0] -= speed
+
+
     def draw(self, ds):
-        ds.blit(self.image,self.image.get_rect())
+        ds.blit(self.image,self.getRect())
--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ pygame.init()
 
 # 创建窗口
 DS = pygame.display.set_mode((1280, 600))
-img = image.Image('pic/other/back.png',(1280, 600))
+img = image.Image('pic/other/back.png',(0,0),(1280, 600))
+img_zb = image.Image('pic/zombie/0/0.png',(1280,200),(100, 128))
 while True:
 
     for event in pygame.event.get():
@@ -16,4 +17,6 @@ while True:
             #R G B
     DS.fill((255, 0, 255))
     img.draw(DS)
+    img_zb.doleft(0.3)
+    img_zb.draw(DS)
     pygame.display.update()
