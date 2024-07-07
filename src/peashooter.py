494553622a2c4fb90bb815bfb34e9d8e1725c00f
centerlv <centerlv@tencent.com>
2024-07-07 21:34:39 +0800

    修复豌豆射手和子弹动画不匹配的问题，save后自动提交

--- src/peashooter.py
+++ src/peashooter.py
@@ -1,12 +1,19 @@
 import objectbase
 import peabullet
+import time
 
 class PeaShooter(objectbase.ObjectBase):
     def __init__(self, id, pos):
         super().__init__(id, pos)
         self.hasBullet=False
+        self.hasShoot=False
     def preSummon(self):
-        self.hasBullet=True
+        """
+        前摇动画，hasShoot为True时才会播放
+        """
+        self.hasShoot=True
+        self.pathIndex=0
+
 
     def hasSummon(self):
         """
@@ -16,4 +23,24 @@ class PeaShooter(objectbase.ObjectBase):
     def doSummon(self):
         if self.hasSummon():
             self.hasBullet=False
-            return peabullet.PeaBulletBase(0,(self.pos[0]+20,self.pos[1]+30))
+            return peabullet.PeaBulletBase(0,(self.pos[0]+self.size[0]-10,self.pos[1]+30))
+        
+    def checkImageIndex(self):
+        """
+        自驱动帧动画
+        """
+        if(time.time()-self.preIndextime)<self.getImageIndexCD():
+            return
+        self.preIndextime=time.time()
+        idx = self.pathIndex+1
+        if idx ==8 and self.hasShoot:
+            self.hasBullet=True
+        if idx >= self.pathIndexCount:
+            """
+            因为8之前都是播放动画的index，所以如果这里超了，无论cd多久，仍然是不播放shoot的动画。
+
+            只要被重置为0后，实际也满足了shoot的条件
+            """
+            idx = 9
+        
+        self.updateIndex(idx)
